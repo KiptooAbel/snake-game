a6c0a5521726fe834e02e110e79a77bb2e4fb0cd
Kiptooabel <4790abz@gmail.com>
2025-12-23 12:02:33 +0300

    Remove babel-plugin-module-resolver - use TypeScript paths instead

--- babel.config.js
+++ babel.config.js
@@ -2,23 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'babel-plugin-module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './'
-          },
-          extensions: [
-            '.js',
-            '.jsx',
-            '.ts',
-            '.tsx',
-            '.json'
-          ]
-        }
-      ]
-    ],
   };
 };
